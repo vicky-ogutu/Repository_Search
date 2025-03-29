17f77ac8b8d5af8ad99a0b0b1cba90b5a8ef7aff
vicky-ogutu <vikogutu77@gmail.com>
2025-03-29 10:02:37 +0300

    ListView

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../Models/repository.dart';
 import '../Provider/repository_provider.dart';
 
 class RepositorySearchScreen extends ConsumerStatefulWidget {
@@ -16,13 +17,13 @@ class _RepositorySearchScreenState extends ConsumerState<RepositorySearchScreen>
   String currentUser = '';
 
 
-  void _search() {
+  void search() {
     final username = searchField.text.trim();
     if (username.isNotEmpty) {
       currentUser = username;
       ref.read(repositoryProvider.notifier).fetchRepositories(username);
     }
-
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +38,29 @@ class _RepositorySearchScreenState extends ConsumerState<RepositorySearchScreen>
         padding: EdgeInsets.all(8),
         child: Column(
           children: [
+            TextField(
+              controller: searchField,
+              decoration: InputDecoration(
+                labelText: "Enter Github Username",
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: search,
+                ),
+              ),
+              onSubmitted: (_) =>search(),
+            ),
+            const SizedBox(height: 8),
+            Expanded(child: repoState.when(
+                data: (data){
+                  return ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index){
+
+                      });
+                },
+                error: (error, stackTrace)=> Text(error.toString()),
+                loading: loading))
 
           ],
         ),
