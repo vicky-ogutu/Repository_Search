577a4bac135a6cedd6b3d40327a6f0383344c447
vicky-ogutu <vikogutu77@gmail.com>
2025-03-29 09:31:15 +0300

    repoScreen_appBar

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../Provider/repository_provider.dart';
 
 class RepositorySearchScreen extends ConsumerStatefulWidget {
   const RepositorySearchScreen({super.key});
@@ -9,8 +12,27 @@ class RepositorySearchScreen extends ConsumerStatefulWidget {
 }
 
 class _RepositorySearchScreenState extends ConsumerState<RepositorySearchScreen> {
+  final TextEditingController searchField = TextEditingController();
+  String currentUser = '';
+
+
+  void _search() {
+    final username = searchField.text.trim();
+    if (username.isNotEmpty) {
+      currentUser = username;
+      ref.read(repositoryProvider.notifier).fetchRepositories(username);
+    }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final repoState = ref.watch(repositoryProvider);
+    return Scaffold(
+      appBar: AppBar(
+      title: Text("Search Repository"),
+
+
+    );
   }
 }
