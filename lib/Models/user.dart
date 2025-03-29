442df4aa37126cd9363587c3be146ec8e7879606
vicky-ogutu <vikogutu77@gmail.com>
2025-03-29 07:37:25 +0300

    add model_class

---
+++ lib/Models/user.dart
@@ -0,0 +1,59 @@
+class User {
+  String? login;
+  int? id;
+  String? nodeId;
+  String? avatarUrl;
+  String? gravatarId;
+  String? url;
+  String? htmlUrl;
+  String? followersUrl;
+  String? followingUrl;
+  String? gistsUrl;
+  String? starredUrl;
+  String? subscriptionsUrl;
+  String? organizationsUrl;
+  String? reposUrl;
+  String? eventsUrl;
+  String? receivedEventsUrl;
+  String? type;
+  String? userViewType;
+  bool? siteAdmin;
+
+User({
+    this.login,
+    this.id,
+    this.nodeId,
+    this.avatarUrl,
+    this.gravatarId,
+    this.url,
+    this.htmlUrl,
+    this.followersUrl,
+    this.followingUrl,
+    this.gistsUrl,
+    this.starredUrl,
+    this.subscriptionsUrl,
+    this.organizationsUrl,
+    this.reposUrl,
+    this.eventsUrl,
+    this.receivedEventsUrl,
+    this.type,
+    this.userViewType,
+    this.siteAdmin,
+  });
+
+
+
+
+factory User.fromJson(Map<String, dynamic> json) => User(
+  avatarUrl: json["avatar_url"],
+  login: json["login"],
+  reposUrl: json["repos_url"]
+);
+
+Map<String, dynamic> toJson()=>{
+  "avatar_url":avatarUrl,
+  "login":login,
+  "repos_url":reposUrl
+};
+
+}
